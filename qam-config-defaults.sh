9b816dbedd27992146ac480a19cc2de82b308665
LABB <unt@labb.email>
2019-03-08 06:09:17 -0500

    Fix default config dir creation script

--- qam-config-defaults.sh
+++ qam-config-defaults.sh
@@ -7,10 +7,14 @@ if [ ! -d "$HOME/.config" ]; then
 	mkdir $defaultConfigFolder
 fi
 
-if [ ! -e "$defaultConfigFolder/dev-labels.conf" ]; then
+if [ ! -d "$defaultConfigFolder" ]; then
+	mkdir $defaultConfigFolder
+fi
+
+if [ ! -f "$defaultConfigFolder/dev-labels.conf" ]; then
 	echo 'devLabelCount=0' > "$defaultConfigFolder/dev-labels.conf"
 fi
 
-if [ ! -e "$defaultConfigFolder/dev-uuids.conf" ]; then
+if [ ! -f "$defaultConfigFolder/dev-uuids.conf" ]; then
 	echo 'devUuidCount=0' > "$defaultConfigFolder/dev-uuids.conf"
 fi
