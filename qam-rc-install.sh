c0a2f9eb0d74a819e4f48752032aa1f6f4a7218b
LABB <unt@labb.email>
2019-03-08 05:47:41 -0500

    Fix permission issues

--- qam-rc-install.sh
+++ qam-rc-install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Verify root
 # TODO verify dmount/dunmount
 # TODO Verify installation
 
 # TODO detect if config is present; ask to override
 
-sudo echo '# QAM RC INSTALL (START) #' >> /rw/config/rc.local
-sudo echo 'qam-startup' >> /rw/config/rc.local
-sudo echo 'sudo -u user qubes-auto-mount' >> /rw/config/rc.local
-sudo echo '# QAM RC INSTALL (END) #' >> /rw/config/rc.local
+echo '# QAM RC INSTALL (START) #' >> /rw/config/rc.local
+echo 'qam-startup' >> /rw/config/rc.local
+echo 'sudo -u user qubes-auto-mount' >> /rw/config/rc.local
+echo '# QAM RC INSTALL (END) #' >> /rw/config/rc.local
--- qam-startup.sh
+++ qam-startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Verify root
 # Verify installation
 
 # TODO Create shutdown/reboot script
 
-sudo echo '#!/bin/bash' > /etc/init.d/qam-shutdown-reboot.sh
-sudo chmod +x /etc/init.d/qam-shutdown-reboot.sh
-sudo echo 'sudo -u user qubes-auto-unmount' >> /etc/init.d/qam-shutdown-reboot.sh
+echo '#!/bin/bash' > /etc/init.d/qam-shutdown-reboot.sh
+chmod +x /etc/init.d/qam-shutdown-reboot.sh
+echo 'sudo -u user qubes-auto-unmount' >> /etc/init.d/qam-shutdown-reboot.sh
 
-sudo ln -s /etc/init.d/qam-shutdown-reboot.sh /etc/rc0.d/000qam
-sudo ln -s /etc/init.d/qam-shutdown-reboot.sh /etc/rc6.d/000qam
+ln -s /etc/init.d/qam-shutdown-reboot.sh /etc/rc0.d/000qam
+ln -s /etc/init.d/qam-shutdown-reboot.sh /etc/rc6.d/000qam
