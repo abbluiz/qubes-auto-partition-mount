f1fc96c6364494c62079a62230874a8a44eb21d3
LABB <unt@labb.email>
2019-03-08 06:13:22 -0500

    Fix update from git command

--- qam-update-from-git.sh
+++ qam-update-from-git.sh
@@ -16,7 +16,37 @@ git clone https://github.com/abbluiz/qubes-auto-partition-mount.git ~/.qam-tmp
 
 # TODO: diff
 
-~/.qam-tmp/qam-install.sh
+installPath="/usr/local/bin"
+
+script[0]="qam-config-defaults"
+script[1]="qam-config"
+script[2]="qam-rc-install"
+script[3]="qam-rc-uninstall"
+script[4]="qam-uninstall"
+script[5]="qam-startup"
+script[6]="qubes-auto-mount"
+script[7]="qubes-auto-unmount"
+script[8]="qam-update-from-git"
+
+updatingOrInstalling() {
+
+    if [ -x "$2/$3" ]; then
+        report="Updating"
+    else
+        report="Installing"
+    fi
+
+}
+
+for i in ${script[@]}
+do 
+
+    report="Installing/Updating"
+    updatingOrInstalling $report $installPath $i
+    echo "$report $i..."
+    install "~/.qam-tmp/$i.sh" "$installPath/$i"
+
+done
 
 echo "Deleting temporary directory $HOME/.qam-tmp"
 rm -rf ~/.qam-tmp
